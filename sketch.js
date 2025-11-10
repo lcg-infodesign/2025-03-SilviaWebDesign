30ddb961e3830d2dbd5664547bc5f56d35a16a99
SilviaWebDesign <silvia.lamastra@mail.polimi.it>
2025-11-11 00:47:37 +0100

    ultime sistemazioni codice

--- sketch.js
+++ sketch.js
@@ -12,16 +12,16 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   
   // deifnisco valori minimi e massimi per la latitudine
-  let tutteLeLatitudini = data.getColumn("Latitude");
+  const tutteLeLatitudini = data.getColumn("Latitude");
   latitudineMinima = min(tutteLeLatitudini);
   latitudineMassima = max(tutteLeLatitudini);
 
   //deifnisco valori minimi e massimi per la lagitudine
-  let tutteLeLongitudini = data.getColumn("Longitude");
+  const tutteLeLongitudini = data.getColumn("Longitude");
   longitudineMinima = min(tutteLeLongitudini);
   longitudineMassima = max(tutteLeLongitudini);
 
-  let tutteLeAltezze = data.getColumn("Elevation (m)");
+  const tutteLeAltezze = data.getColumn("Elevation (m)");
   altezzaMinima = min(tutteLeAltezze);
   altezzaMassima = max(tutteLeAltezze);
   
@@ -33,56 +33,53 @@ function draw() {
   scriviTitolo();
   scriviFooter();
 
-hoveredVolcano = null;
+  vulcanoSelezionato = null;
 
   for (let i=0; i<data.getRowCount(); i++) {
     //leggo i dati della singola riga
-    let riga = data.getRow(i);
-    let longitudine = data.getNum(i, "Longitude");
-    let latitudine = data.getNum(i, "Latitude");
-    let altezza = float(riga.get("Elevation (m)"));
-    let paese = data.getString(i, "Country");
-    let nomeVulcano = riga.get("Volcano Name") || "Unknown";
-    let tipo = riga.get("Type");
-    let status = data.getString(i, "Status");
+    const riga = data.getRow(i);
+    const longitudine = data.getNum(i, "Longitude");
+    const latitudine = data.getNum(i, "Latitude");
+    const altezza = float(riga.get("Elevation (m)"));
+    const paese = data.getString(i, "Country");
+    const nomeVulcano = riga.get("Volcano Name") || "Unknown";
+    const tipo = riga.get("Type");
+    const status = data.getString(i, "Status");
+
     // converto le coordinate geografiche in pixel
     let x = map(longitudine, longitudineMinima, longitudineMassima, margine, width-margine);
     let y = map(latitudine, latitudineMinima, latitudineMassima, height-margine, margine);
-    let activeColor = 100;
-    let dormantColor = 50;
-    let otherColor = null;
-    let trasparenzaVulcano;
-    if (status.includes("Historical") || status.startsWith("D")) {
-        trasparenzaVulcano = activeColor;
-    } else if (status.includes("Holocene") || status === "U") {
-        trasparenzaVulcano = dormantColor;
-    } else {
-        trasparenzaVulcano = otherColor;
-    }
+
+    // definisco trasparenza in base allo stato del vulcano
+    let trasparenzaVulcanoAttivo = 100;
+    let trasparenzaVulcanoDormiente = 50;
+    let altriStatiVulcanici = null;
+    dammiTrasparenzaVulcano(status, trasparenzaVulcanoAttivo, trasparenzaVulcanoDormiente, altriStatiVulcanici);
+    
+    //coloro in vari modi i puntini per rappresentare altezza vulcano
+    //da verde(più basso) a rosso(più alto)
     let coloreAltitudine = map(altezza, altezzaMinima, altezzaMassima, 0, 1);
     let gammaColoriPerAltitudine = lerpColor(color(0, 200, 0, trasparenzaVulcano), color(255, 0, 0, trasparenzaVulcano), coloreAltitudine);
     fill(gammaColoriPerAltitudine);
 
-
-    //fill("yellow");
     let raggio = 5;
 
     // calcolo la distanza
     let distanza = dist(x, y, mouseX, mouseY);
-
-    //if(distanza < raggio) {
-    //  raggio = 10;
-    //} 
-    
+    // se la distanza è minore del raggio aumenta di dimensione
+    if(distanza < raggio) {
+      raggio = 10;
+    } 
+    // disegna ellisse
     ellipse(x, y, raggio*2);
 
     if (distanza < raggio){
       //text(paese, x, y);
-      hoveredVolcano = {paese, nomeVulcano, tipo, altezza, x, y};
+      vulcanoSelezionato = {paese, nomeVulcano, tipo, altezza, x, y};
     }
 
-     // se il vulcano è hovered, disegna un bordo di evidenziazione
-    if (hoveredVolcano && hoveredVolcano.nomeVulcano === nomeVulcano) {
+     // se il vulcano è hovered evidenzia bordo
+    if (vulcanoSelezionato && vulcanoSelezionato.nomeVulcano === nomeVulcano) {
       stroke(200);
       strokeWeight(2);
       ellipse(x, y, raggio*2);
@@ -90,12 +87,43 @@ hoveredVolcano = null;
     }
   }
 
-  // tooltip / label del vulcano sotto il mouse
-  if (hoveredVolcano) {
-    drawTooltip(hoveredVolcano);
+  // tooltip / label descrizione vulcano
+  if (vulcanoSelezionato) {
+    disegnaToolTip(vulcanoSelezionato);
   }
 
-  function drawTooltip(volcano) {
+}
+
+function scriviTitolo() {
+  push();
+  textStyle(BOLD);
+  textAlign(RIGHT, CENTER);
+  textSize(40);
+  fill(250); 
+  text("VULCANI NEL MONDO", width - 100, 60);
+  pop();
+}
+
+function scriviFooter() {
+  textAlign(CENTER, CENTER);
+  textSize(12);
+  fill(250);
+  text("Silvia La mastra — Assignment 03", width / 2, height - 20);
+}
+
+
+function dammiTrasparenzaVulcano(status, trasparenzaVulcanoAttivo, trasparenzaVulcanoDormiente, altriStatiVulcanici) {
+  if (status.includes("Historical") || status.startsWith("D")) {
+        trasparenzaVulcano = trasparenzaVulcanoAttivo;
+    } else if (status.includes("Holocene") || status === "U") {
+        trasparenzaVulcano = trasparenzaVulcanoDormiente;
+    } else {
+        trasparenzaVulcano = altriStatiVulcanici;
+    }
+  return(trasparenzaVulcano);
+}
+
+function disegnaToolTip(volcano) {
   push();
   textSize(12);
   let padding = 8;
@@ -138,25 +166,8 @@ hoveredVolcano = null;
 
   pop();
 }
-  }
+//function coloraVulcaniInBaseAltezza
 
-// per lo stato cambiare opacità
-// se c'è tempo inserire anche una mappa
+//function AdattaTrasparenzaAlloStatus
 
-function scriviTitolo() {
-  push();
-  textStyle(BOLD);
-  textAlign(RIGHT, CENTER);
-  textSize(40);
-  fill(250); 
-  text("VULCANI NEL MONDO", width - 100, 60);
-  pop();
-}
-
-function scriviFooter() {
-  textAlign(CENTER, CENTER);
-  textSize(12);
-  fill(250);
-  text("Silvia La mastra — Assignment 03", width / 2, height - 20);
-}
-
+//function disegnaToolTip
